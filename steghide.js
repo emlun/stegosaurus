9fea5ba9de525cce49faa29306510e3f337da658
Emil Lundberg <lundberg.emil@gmail.com>
2016-01-04 21:22:10 +0100

    Add docstring to steghide.embed

--- steghide.js
+++ steghide.js
@@ -1,6 +1,15 @@
 var child_process = require('child_process');
 var sprintf = require('sprintf').sprintf;
 
+/**
+ * @param imagePath the path to the image to use as the "cover file" -cf
+ *   argument to steghide
+ * @param messagePath the path to the file to use as the "embed file" -ef
+ *   argument to steghide
+ * @param password the password to use for encrypting the message before
+ *   embedding it
+ * @return the path to the created stego file
+ */
 module.exports.embed = function(imagePath, messagePath, password) {
   var steghiddenPath = imagePath + '.steghidden';
 
