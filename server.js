66c91c3f2ce2fc52fcf030be3d190d16b7a89fd8
Emil Lundberg <lundberg.emil@gmail.com>
2016-01-04 21:08:11 +0100

    Extract handle constructor function

--- server.js
+++ server.js
@@ -7,34 +7,50 @@ var app = express();
 
 var steghideEmbed = require('.').steghideEmbed;
 
-app.post('/embed', function(req, res) {
-  var form = new multiparty.Form({
+function steghideEmbedHandler(options) {
+  options = options || {};
+  _.defaults(options, {
     uploadDir: '/tmp/stegosaurus',
+    imageFileParamName: 'the_image',
+    messageFileParamName: 'the_secret',
+    extractionPasswordParamName: 'the_password',
+    responseFileNamePrefix: 'steghidden-',
   });
-  form.parse(req, function(err, fields, files) {
-    if(err) {
-      res.status(401).send('401 Bad request');
-      return false;
-    }
 
-    var imagePath = files.the_image[0].path;
-    var secretPath = files.the_secret[0].path;
-
-    var steghiddenPath = steghideEmbed(
-      imagePath,
-      secretPath,
-      fields.the_password[0]
-    );
-
-    function cleanup(imagePath, secretPath, steghiddenPath) {
-      _([imagePath, secretPath, steghiddenPath]).each(function(path) { fs.unlink(path); });
-    }
-
-    res.download(steghiddenPath, 'steghidden-' + files.the_image[0].originalFilename, function(err) {
-      cleanup(imagePath, secretPath, steghiddenPath);
+  return function(req, res) {
+    var form = new multiparty.Form({
+      uploadDir: options.uploadDir,
     });
-  });
-});
+
+    form.parse(req, function(err, fields, files) {
+      if(err) {
+        res.status(401).send('401 Bad request');
+        console.log('Bad request', err);
+        return false;
+      }
+      var imageFile = files[options.imageFileParamName][0];
+
+      var imagePath = imageFile.path;
+      var secretPath = files[options.messageFileParamName][0].path;
+
+      var steghiddenPath = steghideEmbed(
+        imagePath,
+        secretPath,
+        fields[options.extractionPasswordParamName][0]
+      );
+
+      function cleanup(imagePath, secretPath, steghiddenPath) {
+        _([imagePath, secretPath, steghiddenPath]).each(function(path) { fs.unlink(path); });
+      }
+
+      res.download(steghiddenPath, options.responseFileNamePrefix + imageFile.originalFilename, function(err) {
+        cleanup(imagePath, secretPath, steghiddenPath);
+      });
+    });
+  }
+}
+
+app.post('/embed', steghideEmbedHandler());
 
 app.use(express.static('public'));
 
