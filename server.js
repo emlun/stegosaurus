6fd98ce426a63309bcf4ba85caebfe322db15bb0
Emil Lundberg <emil@emlun.se>
2018-06-23 14:51:59 +0200

    Change server port to 8085

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.post('/embed', stegosaurus.embedHandler());
 app.post('/extract', stegosaurus.extractHandler());
 app.use(express.static('public'));
 
-var server = app.listen(8080, function() {
+var server = app.listen(8085, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Stegosaurus service listening at http://%s:%s', host, port);
